267aa4b020279d381b1728259917a98ad315e85f
Snasy <96841334+snasyde@users.noreply.github.com>
2023-04-11 20:26:56 +0200

    Refactor weather API retrieval code and add error handling
    Updated the code for retrieving weather information from the OpenWeatherMap API to include proper error handling and refactored the code for better readability and maintainability. Added try-except blocks to handle potential exceptions when reading environment variables and making API requests, and provided appropriate error messages for each case.

--- main.py
+++ main.py
@@ -1,26 +1,55 @@
-# Wir importieren das os-Modul, das Funktionen bereitstellt, um auf Umgebungsvariablen und anderen Betriebssystemfunktionen zuzugreifen.
 import os
-
-# Wir importieren die load_dotenv-Funktion aus dem dotenv-Modul, um die Variablen aus der .env-Datei in die Umgebungsvariablen zu setzen.
 from dotenv import load_dotenv
+import requests
 
 # Laden der Umgebungsvariablen aus der .env-Datei
 load_dotenv()
 
-# Wir importieren das requests-Modul, um eine HTTP-Anfrage an die OpenWeatherMap-API zu stellen und Wetterdaten abzurufen.
-import requests
 
+try:
+    #Versuche, die Umgebungsvariablen 'lat' und 'lon' aus der .env-Datei als float-Werte zu lesen
+    lat = float(os.getenv('lat'))
+    lon = float(os.getenv('lon'))
+    
+    #Versuche, die Umgebungsvariablen 'API_KEY' aus der .env-Datei zu lesen
+    api_key = os.getenv('API_KEY')
+    
+except ValueError as e:
+    #Wenn einer der Werte nicht gelesen werden konnte, fange die ValueError-Exception und gib eine Fehlermeldung aus.
+    print('Es gab einen Fehler beim Lesen der Umgebungsvariablen.')
+    print(e)
+    
+except Exception as e:
+    #Wenn ein anderer, nicht erwarteter Fehler auftritt, fange die allgemeine Exception-Exception ab und gib eine Fehlermeldung aus.
+    print('Ein unbekannter Fehler ist aufgetreten.')
+    print(e)
 
-respone = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={float(os.getenv('lat'))}&lon={float(os.getenv('lon'))}&appid={os.getenv('API_KEY')}')
+    
+try:
+    # Versuche, die OpenWeatherMap API-Anfrage durchzuführen
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}')
+    response.raise_for_status()
+    
+    # Extrahiere die notwendigen Daten aus der Antwort
+    data = response.json()
+    location = data['name']
+    weather_description = data['weather'][0]['description']
+    temperature = round(data['main']['temp'] - 273.15)
+    
+    # Gib die Wetterinformationen aus
+    print(f'In {location} sind es {temperature} ℃')
+    print(f'Beschreibung: {weather_description}')
 
-data = respone.json()
-location = data['name']
-weather_description = data['weather'][0]['description']
-temparature = round(data['main']['temp']-273.15)
-
-
-if respone.status_code == 200:
-    print('In '+location+' sind es '+str(temparature)+'℃')
-    print('Beschreibung: '+weather_description)
-else:
-    print("Datenabfrage fehlgeschlagen.")
+except requests.exceptions.HTTPError as e:
+    # Wenn es einen HTTP-Fehler gibt, gebe den Statuscode der Antwort aus
+    print(f'Fehler beim Abrufen der Wetterdaten. Statuscode: {response.status_code}')
+    
+except KeyError as e:
+    # Wenn es einen Fehler bei der Verarbeitung der Daten gibt, gebe den KeyError aus
+    print('Es gab einen Fehler beim Verarbeiten der Wetterdaten.')
+    print(e)
+    
+except Exception as e:
+    # Wenn es einen unbekannten Fehler gibt, gebe eine allgemeine Fehlermeldung aus
+    print('Ein unbekannter Fehler ist aufgetreten.')
+    print(e)
