0291e31d2ea706ff6ea82a55874867b1360f5ed4
Snasy <96841334+snasyde@users.noreply.github.com>
2023-04-12 09:24:47 +0200

    Changed comments language to English
    This commit updates all the comments in the code to English. Previously, the comments were in a different language, which made it difficult for non-native speakers to understand the code. By changing the comments to English, we hope to make the code more accessible to a wider audience and improve its readability. No functional changes have been made to the code itself.

--- main.py
+++ main.py
@@ -2,54 +2,54 @@ import os
 from dotenv import load_dotenv
 import requests
 
-# Laden der Umgebungsvariablen aus der .env-Datei
+# Load environment variables from the .env file
 load_dotenv()
 
-
 try:
-    #Versuche, die Umgebungsvariablen 'lat' und 'lon' aus der .env-Datei als float-Werte zu lesen
+    # Try to read the 'lat' and 'lon' environment variables from the .env file as float values
     lat = float(os.getenv('lat'))
     lon = float(os.getenv('lon'))
     
-    #Versuche, die Umgebungsvariablen 'API_KEY' aus der .env-Datei zu lesen
+    # Try to read the 'API_KEY' environment variable from the .env file
     api_key = os.getenv('API_KEY')
     
 except ValueError as e:
-    #Wenn einer der Werte nicht gelesen werden konnte, fange die ValueError-Exception und gib eine Fehlermeldung aus.
-    print('Es gab einen Fehler beim Lesen der Umgebungsvariablen.')
+    # If either value couldn't be read, catch the ValueError exception and print an error message
+    print('There was an error while reading the environment variables.')
     print(e)
     
 except Exception as e:
-    #Wenn ein anderer, nicht erwarteter Fehler auftritt, fange die allgemeine Exception-Exception ab und gib eine Fehlermeldung aus.
-    print('Ein unbekannter Fehler ist aufgetreten.')
+    # If another unexpected error occurs, catch the general Exception exception and print an error message
+    print('An unknown error occurred.')
     print(e)
 
     
 try:
-    # Versuche, die OpenWeatherMap API-Anfrage durchzuführen
+    # Try to make the OpenWeatherMap API request
     response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}')
     response.raise_for_status()
     
-    # Extrahiere die notwendigen Daten aus der Antwort
+    # Extract the necessary data from the response
     data = response.json()
     location = data['name']
     weather_description = data['weather'][0]['description']
     temperature = round(data['main']['temp'] - 273.15)
     
-    # Gib die Wetterinformationen aus
-    print(f'In {location} sind es {temperature} ℃')
-    print(f'Beschreibung: {weather_description}')
+    # Print the weather information
+    print(f'In {location}, it is {temperature} ℃')
+    print(f'Description: {weather_description}')
 
 except requests.exceptions.HTTPError as e:
-    # Wenn es einen HTTP-Fehler gibt, gebe den Statuscode der Antwort aus
-    print(f'Fehler beim Abrufen der Wetterdaten. Statuscode: {response.status_code}')
+    # If there's an HTTP error, print the response status code
+    print(f'Error while retrieving weather data. Status code: {response.status_code}')
     
 except KeyError as e:
-    # Wenn es einen Fehler bei der Verarbeitung der Daten gibt, gebe den KeyError aus
-    print('Es gab einen Fehler beim Verarbeiten der Wetterdaten.')
+    # If there's an error processing the data, print the KeyError
+    print('There was an error while processing the weather data.')
     print(e)
     
 except Exception as e:
-    # Wenn es einen unbekannten Fehler gibt, gebe eine allgemeine Fehlermeldung aus
-    print('Ein unbekannter Fehler ist aufgetreten.')
+    # If there's an unknown error, print a general error message
+    print('An unknown error occurred.')
     print(e)
+
